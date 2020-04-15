f34f6b6c2857d7ed355f54a69ea905bd904fa591
Oliver Kind <github@oliver-kind.de>
2020-04-15 20:43:52 +0200

    Modified for new ExplorerTreeView

--- src/MainForm/clsExtendedTreeNode.cs
+++ src/MainForm/clsExtendedTreeNode.cs
@@ -29,24 +29,32 @@ namespace OLKI.Programme.QBC.MainForm
     /// <summary>
     /// Enhanced the TreeViewNode with an directroy info and and an half automatic icon
     /// </summary>
-    internal class ExtendedTreeNode : System.Windows.Forms.TreeNode
+    public class ExtendedTreeNode : System.Windows.Forms.TreeNode
     {
         #region Constants
         /// <summary>
         /// Specifies the default variant of the Icon for this TreeNode
         /// </summary>
-        private const ExtendedTreeNodeCheckedState DEFAULT_IMAGE_VARIANT = ExtendedTreeNodeCheckedState.Question;
+        private const CheckedState DEFAULT_IMAGE_VARIANT = CheckedState.Question;
         /// <summary>
-        /// Specifies the default  base icon this TreeNode
+        /// Specifies the default  base icon of this TreeNode
         /// </summary>
         private const int DEFAULT_BASE_ICON = -1;
+        /// <summary>
+        /// The format if a directroy is a drive
+        /// </summary>
+        private const string DRIVE_NAME_FORMAT = @"{0} ({1})";
+        /// <summary>
+        /// Text to display for dummy tree nodes
+        /// </summary>
+        private const string DUMMY_IDENTIFIER = @"\\DUMMY//";
         #endregion
 
         #region Enums
         /// <summary>
-        /// Represents the check state variant of a TreeNodePlus icon
+        /// Represents the check state variant of a ExtendedTreeNode icon
         /// </summary>
-        public enum ExtendedTreeNodeCheckedState
+        public enum CheckedState
         {
             /// <summary>
             /// Check state icon is an questionmark icon
@@ -90,6 +98,27 @@ namespace OLKI.Programme.QBC.MainForm
         }
 
         /// <summary>
+        /// Specifies the variant of the Icon for this TreeNode
+        /// </summary>
+        private CheckedState _imageVariant = DEFAULT_IMAGE_VARIANT;
+        /// <summary>
+        /// Get or set the variant of the Icon for this TreeNode
+        /// </summary>
+        public CheckedState ImageVariant
+        {
+            get
+            {
+                return this._imageVariant;
+            }
+            set
+            {
+                this._imageVariant = value;
+                base.ImageIndex = (int)(this._baseImageIndex + this._imageVariant);
+                base.SelectedImageIndex = base.ImageIndex;
+            }
+        }
+
+        /// <summary>
         /// Specifies the information of the directroy associated with this TreeNode
         /// </summary>
         private readonly DirectoryInfo _directoryInfo = null;
@@ -105,36 +134,63 @@ namespace OLKI.Programme.QBC.MainForm
         }
 
         /// <summary>
-        /// Specifies the variant of the Icon for this TreeNode
+        /// Indicades if the node is an dummy node, for more sub nodes at an paren note
         /// </summary>
-        private ExtendedTreeNodeCheckedState _imageVariant = DEFAULT_IMAGE_VARIANT;
+        private readonly bool _isDummy = false;
+
         /// <summary>
-        /// Get or set the variant of the Icon for this TreeNode
+        /// Get if the node is an dummy node, for more sub nodes at an paren note
         /// </summary>
-        public ExtendedTreeNodeCheckedState ImageVariant
+        public bool IsDummy
         {
             get
             {
-                return this._imageVariant;
-            }
-            set
-            {
-                this._imageVariant = value;
-                base.ImageIndex = (int)(this._baseImageIndex + this._imageVariant);
-                base.SelectedImageIndex = base.ImageIndex;
+                return this._isDummy;
             }
         }
         #endregion
 
         #region Methodes
         /// <summary>
-        /// Initialise a new TreeNodePlus with a specified directroy
+        /// Initialise a new ExtendedTreeNode with a specified directroy
         /// </summary>
         /// <param name="directoryInfo">Specifies the directroy, associated to the tree node</param>
-        public ExtendedTreeNode(DirectoryInfo directoryInfo)
+        public ExtendedTreeNode(DirectoryInfo directoryInfo) : this(directoryInfo, false)
+        {
+        }
+
+        /// <summary>
+        /// Initialise a new ExtendedTreeNode with a specified directroy
+        /// </summary>
+        /// <param name="directoryInfo">Specifies the directroy, associated to the tree node</param>
+        /// <param name="isDummy">Specifies if the ExtendedTreeNode is used as dummy</param>
+        public ExtendedTreeNode(DirectoryInfo directoryInfo, bool isDummy)
         {
             this._directoryInfo = directoryInfo;
-            base.Text = this._directoryInfo.FullName.Substring(this._directoryInfo.FullName.LastIndexOf(@"\") + 1);
+            this._isDummy = isDummy;
+            base.Text = this.GetNodeText(directoryInfo, isDummy);
+        }
+
+        /// <summary>
+        /// Create the text of the TreeNode
+        /// </summary>
+        /// <param name="directoryInfo">The directory to create the name from</param>
+        /// <param name="isDummy">Is the directroy a dummy directory</param>
+        /// <returns>The text to name the directroy</returns>
+        private string GetNodeText(DirectoryInfo directoryInfo, bool isDummy)
+        {
+            // Node is dummy
+            if (isDummy) return DUMMY_IDENTIFIER;
+
+            // Node is drive
+            if (Tools.CommonTools.DirectoryAndFile.Path.IsDrive(directoryInfo))
+            {
+                DriveInfo Drive = new DriveInfo(directoryInfo.FullName);
+                return string.Format(DRIVE_NAME_FORMAT, new object[] { Drive.IsReady ? Drive.VolumeLabel : string.Empty, Drive.Name });
+            }
+
+            // Node is directory
+            return directoryInfo.Name;
         }
         #endregion
     }
