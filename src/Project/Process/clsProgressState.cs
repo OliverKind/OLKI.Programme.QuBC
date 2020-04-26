45c6ad140f47961349dcfeececc2db8f71a6f6b0
Oliver Kind <github@oliver-kind.de>
2020-04-26 17:37:47 +0200

    Initial commit

---
+++ src/Project/Process/clsProgressState.cs
@@ -0,0 +1,60 @@
+﻿/*
+ * QBC- QuickBackupCreator
+ * 
+ * Copyright:   Oliver Kind - 2020
+ * License:     LGPL
+ * 
+ * Desctiption:
+ * A class to send the progress state
+ * 
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LGPL General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not check the GitHub-Repository.
+ * 
+ * */
+
+namespace OLKI.Programme.QBC.BackupProject.Process
+{
+    public class ProgressState
+    {
+        #region Constants
+        private const bool DEFAULT_FORCE_REPORT_PROGRRESS = false;
+        #endregion
+
+        #region Properties
+        private readonly ProgressStore _progressStore;
+        public ProgressStore ProgressStore
+        {
+            get
+            {
+                return this._progressStore;
+            }
+        }
+
+        public bool ForceReportProgress { get; set; } = DEFAULT_FORCE_REPORT_PROGRRESS;
+        #endregion
+
+        #region Methodes
+        public ProgressState(ProgressStore progressStore) : this(progressStore, DEFAULT_FORCE_REPORT_PROGRRESS)
+        {
+        }
+        public ProgressState(bool forceReportProgress) : this(null, forceReportProgress)
+        {
+        }
+        public ProgressState(ProgressStore progressStore, bool forceReportProgress)
+        {
+            this._progressStore = progressStore.Clone();
+            this.ForceReportProgress = forceReportProgress;
+        }
+        #endregion
+    }
+}
