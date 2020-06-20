d3f9752d81e6e763eeb232b942f997584737190d
Oliver Kind <github@oliver-kind.de>
2020-06-20 14:14:53 +0200

    Updated to version 1.1.2.9

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("QBC- QuickBackupCreator")]
+[assembly: AssemblyTitle("QBC - QuickBackupCreator")]
 [assembly: AssemblyDescription("Ein Programm um schnell Datensicherungen zu erstellen")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("OLKI-Software")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1.1.8")]
-[assembly: AssemblyFileVersion("1.1.1.8")]
+[assembly: AssemblyVersion("1.1.2.9")]
+[assembly: AssemblyFileVersion("1.1.2.9")]
