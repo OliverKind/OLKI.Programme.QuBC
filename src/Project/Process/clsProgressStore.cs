553392b87002ab5af09908c862c8c81d69e68d60
Oliver Kind <github@oliver-kind.de>
2020-10-18 15:42:49 +0200

    Add comments

--- src/Project/Process/clsProgressStore.cs
+++ src/Project/Process/clsProgressStore.cs
@@ -24,12 +24,29 @@
 
 namespace OLKI.Programme.QBC.src.Project.Process
 {
+    /// <summary>
+    /// A class to store the progess of an backup or restore process
+    /// </summary>
     public class ProgressStore
     {
         #region Properties
+        /// <summary>
+        /// Total files counted or copied
+        /// </summary>
         public ProgressElement TotalFiles { get; set; } = new ProgressElement();
+
+        /// <summary>
+        /// Total directories counted or copied
+        /// </summary>
         public ProgressElement TotalDirectories { get; set; } = new ProgressElement();
+        /// <summary>
+        /// Total bytes counted or copied
+        /// </summary>
         public ProgressElement TotalBytes { get; set; } = new ProgressElement();
+
+        /// <summary>
+        /// Total bytes counted or copied
+        /// </summary>
         public ProgressElement TotalItems
         {
             get
@@ -42,13 +59,27 @@ namespace OLKI.Programme.QBC.src.Project.Process
                 return TotalItems;
             }
         }
+
+        /// <summary>
+        /// Files in the actual copied directroy
+        /// </summary>
         public ProgressElement DirectroyFiles { get; set; } = new ProgressElement();
+
+        /// <summary>
+        /// Bytes of the actual copied file
+        /// </summary>
         public ProgressElement FileBytes { get; set; } = new ProgressElement();
 
+        /// <summary>
+        /// Exception while copy or count a item
+        /// </summary>
         public ProcessException Exception { get; set; } = new ProcessException();
         #endregion
 
         #region Methodes
+        /// <summary>
+        /// Initial a new ProgressStore
+        /// </summary>
         public ProgressStore()
         {
             this.TotalBytes = new ProgressElement();
@@ -57,6 +88,10 @@ namespace OLKI.Programme.QBC.src.Project.Process
             this.DirectroyFiles = new ProgressElement();
         }
 
+        /// <summary>
+        /// Clone the ProgressStore
+        /// </summary>
+        /// <returns>The cloned ProgressStore</returns>
         public ProgressStore Clone()
         {
             ProgressStore ThisClone = (ProgressStore)this.MemberwiseClone();
@@ -65,13 +100,30 @@ namespace OLKI.Programme.QBC.src.Project.Process
         #endregion
 
         #region Subclasses
+        /// <summary>
+        /// A class to store the progess data of a single element
+        /// </summary>
         public class ProgressElement
         {
             #region Properties
+            /// <summary>
+            /// Actual counted or copied value, Bytes for example
+            /// </summary>
             public long? ActualValue { get; set; } = null;
+
+            /// <summary>
+            /// Maximum counted or copied value, Bytes for example
+            /// </summary>
             public long? MaxValue { get; set; } = null;
+
+            /// <summary>
+            /// The name of the actual element, Filename for example
+            /// </summary>
             public string ElemenName { get; set; } = string.Empty;
 
+            /// <summary>
+            /// The percentage value of the actual copy progress, Byte of an file for example
+            /// </summary>
             public int Percentage
             {
                 get
