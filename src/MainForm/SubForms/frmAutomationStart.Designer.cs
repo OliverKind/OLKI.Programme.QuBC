d418b905ef6cf7018b4de75d87de9a9588bcb3a7
Oliver Kind <github@oliver-kind.de>
2022-02-05 13:18:12 +0100

    Initial commit

---
+++ src/MainForm/SubForms/frmAutomationStart.Designer.cs
@@ -0,0 +1,90 @@
+﻿
+namespace OLKI.Programme.QuBC.src.MainForm.SubForms
+{
+    partial class AutomationStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.btnCancel.Location = new System.Drawing.Point(244, 41);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(110, 23);
+            this.btnCancel.TabIndex = 13;
+            this.btnCancel.Text = "&Abbrechen";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 41);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(110, 23);
+            this.btnStart.TabIndex = 12;
+            this.btnStart.Text = "&Starten";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(12, 12);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(342, 23);
+            this.progressBar1.TabIndex = 14;
+            // 
+            // AutomationStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(366, 76);
+            this.ControlBox = false;
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnStart);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AutomationStart";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Starten Automatisation: {0}";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
---
+++ src/MainForm/SubForms/frmAutomationStart.cs
@@ -0,0 +1,125 @@
+﻿/*
+ * QuBC - QuickBackupCreator
+ * 
+ * Initial Author: Oliver Kind - 2021
+ * License:        LGPL
+ * 
+ * Desctiption:
+ * The AutomationStart Formn, to wait until the Automation starts
+ * 
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the LGPL General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * LGPL General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not check the GitHub-Repository.
+ * 
+ * */
+
+using System;
+using System.Windows.Forms;
+
+namespace OLKI.Programme.QuBC.src.MainForm.SubForms
+{
+    /// <summary>
+    /// The AutomationStart Formn, to wait until the Automation starts
+    /// </summary>
+    public partial class AutomationStart : Form
+    {
+        #region Fields
+        /// <summary>
+        /// Application MainForm
+        /// </summary>
+        private readonly MainForm _mainForm;
+
+        /// <summary>
+        /// Project common settings, to get automation settings
+        /// </summary>
+        readonly Project.Settings.Common.Common _settings;
+
+        /// <summary>
+        /// Timer to start the automation
+        /// </summary>
+        private readonly Timer _timer = new Timer();
+
+        /// <summary>
+        /// Count the timer Ticks, to start the automation
+        /// </summary>
+        private int _tickCounter = 0;
+        #endregion
+
+        #region Methodes
+        /// <summary>
+        /// Initialise a new AutomationStart Form
+        /// </summary>
+        /// <param name="mainForm">Application MainForm</param>
+        /// <param name="projectCommonSettings">Project common settings, to get automation settings</param>
+        public AutomationStart(MainForm mainForm, Project.Settings.Common.Common projectCommonSettings)
+        {
+            InitializeComponent();
+
+            this._mainForm = mainForm;
+            this._settings = projectCommonSettings;
+
+            this.progressBar1.Maximum = this._settings.AutomationWaitTime * 10;
+            this._timer.Tick += new EventHandler(this.timer_Tick);
+
+
+            switch (this._settings.Automation)
+            {
+                case Project.Settings.Common.Common.AutomationMode.Backup:
+                    this.Text = string.Format(this.Text, Properties.Stringtable._0x002C);
+                    this._mainForm.tabControlMain.SelectTab(1);
+                    this._timer.Start();
+                    break;
+                case Project.Settings.Common.Common.AutomationMode.Restore:
+                    this.Text = string.Format(this.Text, Properties.Stringtable._0x002C);
+                    this._mainForm.tabControlMain.SelectTab(2);
+                    this._timer.Start();
+                    break;
+                default:
+                    this.Close();
+                    break;
+            }
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            this._tickCounter++;
+            if (this._tickCounter <= this._settings.AutomationWaitTime * 10) this.progressBar1.Value = this._tickCounter;
+            if (this._tickCounter == this._settings.AutomationWaitTime * 10) this.btnStart_Click(sender, e);
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this._timer.Stop();
+            this.Close();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            this._timer.Stop();
+            this.Close();
+
+            switch (this._settings.Automation)
+            {
+                case Project.Settings.Common.Common.AutomationMode.Backup:
+                    this._mainForm.uscTaskControleBackup.btnTaskStart_Click(sender, e);
+                    break;
+                case Project.Settings.Common.Common.AutomationMode.Restore:
+                    this._mainForm.uscTaskControleRestore.btnTaskStart_Click(sender, e);
+                    break;
+                default:
+                    this.Close();
+                    break;
+            }
+        }
+        #endregion
+    }
+}
